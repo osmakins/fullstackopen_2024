b2544bbc83a78db1611954e6e5258bf13ca4261a
M.O Akindayo <matthewakinse@gmail.com>
2024-04-04 05:39:45 +0300

    Ex.1.2: course information, step 2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,6 +1,17 @@
-const Content = (props) => {
+
+const Part = (props) =>{
     return ( 
         <p>{props.part} {props.exercise}</p>
+     );
+}
+
+const Content = (props) => {
+    return ( 
+        <div>
+            <Part part={props.part1} exercise={props.exercise1}/>
+            <Part part={props.part2} exercise={props.exercise2}/>
+            <Part part={props.part3} exercise={props.exercise3}/>
+        </div>
      );
 }
  
