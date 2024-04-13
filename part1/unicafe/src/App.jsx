4ce050970db80b2a7ed4c31fd64c28af3cc3fdc1
M.O Akindayo <matthewakinse@gmail.com>
2024-04-13 03:28:09 +0300

    x.1.7: unicafe, step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,18 +6,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = bad + neutral + good;
+  const average = all/3;
+  const positives = all ? (good/all) * 100 : 0 // Condition to avoid NaN divide by zero
+
   return (
-    <div>
-      <h3>give feedback</h3>
+    <div className="App">
+      <h2>Give Feedback</h2>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h3>statistics</h3>
+      <h2>Statistics</h2>
       <span>good: {good}</span><br/>
       <span>neutral: {neutral}</span><br/>
-      <span>bad: {bad}</span>
+      <span>bad: {bad}</span><br/>
+      <span>all: {all}</span><br/>
+      <span>average: {average}</span><br/>
+      <span>positive: {positives}%</span>
     </div>
-  )
+  );
 }
 
 export default App
