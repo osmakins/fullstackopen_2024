d0f701b2701639f582e49ae6f51f9a4637ae6e54
M.O Akindayo <matthewakinse@gmail.com>
2024-04-21 18:09:46 +0000

    x.2.2: Course information, step 7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -4,12 +4,18 @@ const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) => <>{parts.map(part=><Part key={part.id} part={part} />)}</>
 
+const Total = ({parts}) => {
+  const total = parts.map(part => part.exercises).reduce((a, b)=>a+b)
+  return(<><p>Total of {total} exercises</p></>)
+}
+
 const Course = ({course})=>{
 
   return(
     <>
       <Header header={course.name} />
-      <Content parts={course.parts} />  
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
